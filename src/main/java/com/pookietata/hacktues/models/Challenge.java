275119a911442e3565a542c32308f18e0bb568b4
Yavor Simeonov <yavorjsimeonov@gmail.com>
2024-03-14 14:38:00 +0200

    Finished the model

---
+++ src/main/java/com/pookietata/hacktues/models/Challenge.java
@@ -0,0 +1,65 @@
+package com.pookietata.hacktues.models;
+
+import com.pookietata.hacktues.models.enums.ChallengeType;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Challenges")
+public class Challenge {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long challenge_id;
+
+  @NonNull
+  @NotBlank
+  @Column(nullable = false, name = "challenge_type")
+  private ChallengeType challengeType;
+
+  @NonNull
+  @NotBlank
+  @Column(name = "Reward")
+  private int rewardValue;
+
+  @NonNull
+  @NotBlank
+  @Column(name = "Name")
+  private String name;
+
+  @NonNull
+  @NotBlank
+  @Column(name = "Description", length = 1024)
+  private String description;
+
+  @NonNull
+  @NotBlank
+  @Column(name = "Expiration_time")
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date expirationTime;
+
+  @NonNull
+  @NotBlank
+  @Column(name = "Start_time")
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date startTime;
+
+  @NonNull
+  @NotBlank
+  @Column(name = "Entry_fee")
+  private int entryFee;
+
+  @OneToMany(mappedBy = "challenge", cascade = CascadeType.ALL, orphanRemoval = true)
+  private Set<Reward> rewards = new HashSet<>();
+
+
+}
+
---
+++ src/main/java/com/pookietata/hacktues/models/Reward.java
@@ -0,0 +1,29 @@
+package com.pookietata.hacktues.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Reward")
+public class Reward {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NonNull
+  @NotBlank
+  @Column(name = "Value")
+  private int rewardValue;
+
+  @ManyToOne(fetch = FetchType.LAZY) // Lazy loading is often a sensible default
+  @JoinColumn(name = "challenge_id") // This defines the foreign key column.
+  private Challenge challenge;
+}
+
