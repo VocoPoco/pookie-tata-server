1cd2040fb1b1dc811e50d7839130f5743bb004dc
VocoPoco <VocoPoco@email.com>
2024-03-14 14:14:46 +0200

    added repository annotations

--- src/main/java/com/pookietata/hacktues/repositories/UserRepository.java
+++ src/main/java/com/pookietata/hacktues/repositories/UserRepository.java
@@ -5,9 +5,12 @@ import com.pookietata.hacktues.models.User;
 import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.username = :username")
     Optional<User> findUserByUsername(String username);
